6783c9a681d60f0a3b772b05803128078be28b95
Igbal <igbal.alyasov@mail.ru>
2024-11-28 01:58:03 +0300

    Создал функцию добавление книг в библиотеку, добавил функцию очищения формы, добавил функцию добавления книг в html, а так же добавил стили к контейнеру книг в html

--- script.js
+++ script.js
@@ -1,9 +1,73 @@
 let myLibrary = [];
 
-function Book () {
-
+function Book (author, name, number_of_pages) {
+    this.author = author;
+    this.name = name;
+    this.number_of_pages = number_of_pages;
 }
 
-function addBooktoLibrary (book) {
-
+function resetForm () {
+    document.querySelector('#author').value = '';
+    document.querySelector('#name').value = '';
+    document.querySelector('#number_of_pages').value = '';
 }
+
+function addBookToHtml () {
+    let containerBook = document.querySelector('.container__books');
+    let blockBook = document.createElement('div');
+    blockBook.setAttribute('class', 'block__book')
+
+    blockBook.innerHTML = `
+        <div class="display__book">
+            <div class="block__book__content">
+                <div class="number__book">1</div>
+                <div class="name__book">Name</div>
+            </div>
+        </div>
+
+        <div class="description__book">
+            <div class="block__discription">
+                <div class="title_description">Author:</div>
+                <div class="value__description"></div>
+            </div>
+            <div class="block__discription">
+                <div class="title_description">Name:</div>
+                <div class="value__description"></div>
+            </div>
+            <div class="block__discription">
+                <div class="title_description">Number of pages:</div>
+                <div class="value__description"></div>
+            </div>
+            <div class="block__discription">
+                <div class="title_description">Read:</div>
+                <div class="value__description"></div>
+            </div>
+        </div>
+
+        <button class="delete" type="submit">Delete</button>
+    `;
+
+    containerBook.appendChild(blockBook);
+}
+
+function addBooktoLibrary () {
+    let submit = document.querySelector('.submit');
+    submit.addEventListener('click', function (event) {
+        let author = document.querySelector('#author').value;
+        let nameBook = document.querySelector('#name').value;
+        let numberOfPages = document.querySelector('#number_of_pages').value;
+        
+        const book1 = new Book(author, nameBook, numberOfPages);
+        myLibrary.push(book1)
+
+        console.log(myLibrary);
+        
+        resetForm();
+        addBookToHtml();
+        event.preventDefault();
+    });
+}
+
+
+
+addBooktoLibrary();
